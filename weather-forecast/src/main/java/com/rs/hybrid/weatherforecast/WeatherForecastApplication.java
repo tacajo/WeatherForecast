75da4ce0d407355094062ff4666f5702fb91599b
tacajo <tacajovicic@gmail.com>
2020-07-21 18:36:07 +0200

    init

---
+++ weather-forecast/src/main/java/com/rs/hybrid/weatherforecast/WeatherForecastApplication.java
@@ -0,0 +1,13 @@
+package com.rs.hybrid.weatherforecast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherForecastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherForecastApplication.class, args);
+	}
+
+}
