6c4bfdb3b8d787a35ea106e5231b8e1268ddbec9
tacajo <tacajovicic@gmail.com>
2020-07-24 00:19:01 +0200

    bug-fix

--- weather-forecast/src/main/java/com/rs/hybrid/weatherforecast/service/WeatherForecastServiceImpl.java
+++ weather-forecast/src/main/java/com/rs/hybrid/weatherforecast/service/WeatherForecastServiceImpl.java
@@ -84,7 +84,7 @@ public class WeatherForecastServiceImpl implements WeatherForecastService {
         HttpEntity<APIResponse> response = restTemplate.getForEntity(
                 builder.toUriString(),
                 APIResponse.class);
-        System.out.println(builder.toUriString());
+
         City city = conversionService.convert(response.getBody().getCity(), City.class);
         city.setCoord(coordinateService.save(conversionService.convert(response.getBody().getCity().getCoord(), Coordinate.class)));
 
